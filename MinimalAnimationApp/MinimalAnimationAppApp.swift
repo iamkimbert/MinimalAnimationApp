340ac6a37d5b914c794d77a309b5c55d864af7d9
iamkimbert <iamkimbert@gmail.com>
2022-09-16 10:47:33 -0400

    Initial Commit

---
+++ MinimalAnimationApp/MinimalAnimationAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MinimalAnimationAppApp.swift
+//  MinimalAnimationApp
+//
+//  Created by Kimberly Townsend on 9/16/22.
+//
+
+import SwiftUI
+
+@main
+struct MinimalAnimationAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
